3216788d7591546070e0af4d10735fb3aeb2832a
nicolò andreozzi <nicolo.andreozzi@brumbrum.it>
2022-10-21 01:10:38 +0200

    docker with nest.js typescript vue vite

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Response } from 'express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(@Res() res: Response) {
+    return res.status(HttpStatus.OK).json(this.appService.getHello());
+  }
+}
---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true,
+    port: 8080,
+    watch: {
+      usePolling: true,
+    },
+  }
+})
